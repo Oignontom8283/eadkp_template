adbc1d37329f75a526b226b80dead276ecfcf7d9
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-01-18 02:01:35 +0100

    fix: update shell.sh to use dynamic project name from Cargo.toml

--- shell.sh
+++ shell.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Get the project name from Cargo.toml
+PROJECT_NAME=$(cargo metadata --format-version 1 --no-deps | jq -r '.packages[0].name')
+
 # Allow local connections to the X server
 xhost +local:docker
 
-docker exec -it {{project-name}} bash
+docker exec -it "$PROJECT_NAME" bash
