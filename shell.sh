22f65262b6cdc6e52e070366f9b0b5c83ed71f51
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-01-19 00:20:32 +0100

    fix: revert project name retrieval in shell.sh to use placeholder

--- shell.sh
+++ shell.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Get the project name from Cargo.toml
-PROJECT_NAME=$(cargo metadata --format-version 1 --no-deps | jq -r '.packages[0].name')
-
 # Allow local connections to the X server
 xhost +local:docker
 
-docker exec -it "$PROJECT_NAME" bash
+docker exec -it {{project-name}} bash
