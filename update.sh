bae95819a34be1e8493801d09a35c9dec7b4ab66
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-04-10 17:49:31 +0200

    refactor: streamline script execution by centralizing internal script calls

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
-
 #!/bin/bash
+
+# Target directory containing the internal scripts
+TARGET_DIR=".eadkp"
+TARGET_SCRIPT="$TARGET_DIR/$(basename "$0")"
 
 # Move to the script's directory in case it is called from another location
 cd "$(dirname "$0")"
 
-# Call update script for sync
-source ./.eadkp/update.sh
+# Pass execution to the internal script and forward any given arguments
+exec "./$TARGET_SCRIPT" "$@"
